3144b43fb66a22c213e99792bbaf6077e1569e0c
DesaiPriyansh19 <priyanshdesai62@gmail.com>
2024-12-11 10:18:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return(<>
+    <div className="min-h-screen flex items-center justify-center bg-gray-800">
+      <h1 className="text-5xl font-bold text-blue-800">Hello, Tailwind CSS!</h1>
+    </div>
+  </>)
+}
+
+export default App

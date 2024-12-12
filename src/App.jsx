832ee34770bde2d882c1a6f9d669ba2cbb704b50
DesaiPriyansh19 <priyanshdesai62@gmail.com>
2024-12-12 14:39:33 +0530

    Login popup added

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,48 @@ import room from "../src/assets/Room3.jpg";
 import Cards from "./components/Cards";
 import WhyShould from "./components/WhyShould";
 import { AnimatePresence } from "framer-motion";
-
-const images = [Img1, room];
+import LoginPopup from "./components/LoginPopup";
+const images = [room,Img1];
 
 const dynamicContent = [
-  {
-    title: "Welcome to Millennium Properties",
-    description: "We Build Your Dreams",
-  },
   {
     title: "Luxury Apartment",
     description: "Your Dreams, Our Vision; A Sustainable Future",
   },
+  {
+    title: "Welcome to Millennium Properties",
+    description: "We Build Your Dreams",
+  },
+
 ];
 
 function App() {
   const [isVisible, setIsVisible] = useState(true);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const handlePopupOpen = () => setIsPopupOpen(true);
+  const handlePopupClose = () => setIsPopupOpen(false);
 
   useEffect(() => {
     // Hide the animation after 3 seconds with smooth fade out
     const timer = setTimeout(() => {
       setIsVisible(false);
-    }, 3000); // Adjust time as needed
+    }, 2000); // Adjust time as needed
 
     return () => clearTimeout(timer); // Cleanup the timer
   }, []);
 
   return (
+    
     <>
        <AnimatePresence>
         {isVisible && <LoaderAnimation key="loader" />}
       </AnimatePresence>
       <div className="font-roboto h-full p-0 m-0 w-full">
+    
+     
         <Navbar />
+        {/* <LoginPopup   /> */}
         <Carousel images={images} dynamicContent={dynamicContent} />
         <Cards />
         <WhyShould />

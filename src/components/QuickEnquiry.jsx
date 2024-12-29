4d326af40042beecf73a1039b5b46fbc6282896c
karan <jangdekaran54@gmail.com>
2024-12-29 17:36:16 +0530

    QuickEnquiryUpdated

--- src/components/QuickEnquiry.jsx
+++ src/components/QuickEnquiry.jsx
@@ -1,110 +1,161 @@
-import React,{ Component } from 'react';
-import 'tailwindcss/tailwind.css';
-import img from '../assets/support.jpg'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "tailwindcss/tailwind.css";
+import img from "../assets/support.jpg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import InputField from "../../utils/InputFields";
+import axios from "axios";
 const QuickEnquiry = () => {
-  return (<>
-  <div style={{ backgroundImage: `url(${img})` }}
-     className="w-full h-[400px] sm:h-[300px] md:h-[350px] lg:h-[300px] bg-cover bg-center bg-no-repeat relative 
-     before:absolute before:inset-0 before:bg-black/10 before:backdrop-blur-[0px]">
-               <div className="absolute inset-0 bg-black/35 md:bg-black/45"></div>
-               <div className="  relative z-10 flex flex-col items-center justify-center 
-               text-center text-white h-full px-4" style={{
-                textShadow: "4px 4px 8px rgba(2, 2, 0.5, 0.5)", // Apply text shadow to all children
-              }} >
-            <h2 className=" mt-10 text-2xl md:text-2xl lg:text-5xl font-normal">
+  const [formData, setFormData] = useState({
+    EnquiryPersonName: "",
+    EnquiryPersonEmail: "",
+    EnquiryPersonPhone: "",
+    EnquiryPersonMessage: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { value, name } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "https://milaniumepropertybackend.vercel.app/api/enquiry",
+        formData
+      );
+      console.log("Response:", response.data);
+      alert("Enquiry submitted successfully!");
+      setFormData({
+        EnquiryPersonName: "",
+        EnquiryPersonEmail: "",
+        EnquiryPersonPhone: "",
+        EnquiryPersonMessage: "",
+      });
+    } catch (error) {
+      console.error("Error submitting enquiry:", error);
+      alert(
+        "An error occurred while submitting your enquiry. Please try again."
+      );
+    }
+  };
+  return (
+    <>
+      <div
+        style={{ backgroundImage: `url(${img})` }}
+        className="w-full h-[400px] sm:h-[300px] md:h-[350px] lg:h-[300px] bg-cover bg-center bg-no-repeat relative 
+     before:absolute before:inset-0 before:bg-black/10 before:backdrop-blur-[0px]"
+      >
+        <div className="absolute inset-0 bg-black/35 md:bg-black/45"></div>
+        <div
+          className="  relative z-10 flex flex-col items-center justify-center 
+               text-center text-white h-full px-4"
+          style={{
+            textShadow: "4px 4px 8px rgba(2, 2, 0.5, 0.5)", // Apply text shadow to all children
+          }}
+        >
+          <h2 className=" mt-10 text-2xl md:text-2xl lg:text-5xl font-normal">
             Quick Enquiry
-            </h2>
-            <p className="hidden md:flex mt-4 font-thin text-sm px-40">
-            Whether you’re looking to buy, sell, or rent, our experienced team is here to guide you through <br></br> every step of the process. Fill out the form below,and we’ll get <br></br> back to you promptly with personalized assistance.
-
-
-            </p>
-        <Link to={'/'}  >  <button className="mt-6 px-4 py-2 bg-[#E7C873] text-sm text-black rounded-lg font-normal">
-             Back to Home &#8594;
-            </button></Link>
-          </div>
-     </div>
-    <div className="flex flex-wrap bg-white p-8"
-    >
-      {/* Left Section - Form Inputs */}
-      
-      <div className="w-full md:w-1/2 p-4">
-        <h2 className="text-2xl font-semibold mb-4">Our Office Location</h2>
-        <p>C-401, Ganesh Glory 11,Nr. Moneyplant Highstreet,Jagatpur Road, Gota, Ahmedabad-382481</p>
-        <div className="map-container w-full h-64">
-          {/* Replace the coordinates with your office's latitude and longitude */}
-          <iframe 
-    src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d7339.078095836263!2d72.538186!3d23.113964!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDA2JzUwLjMiTiA3MsKwMzInMjYuNyJF!5e0!3m2!1sen!2sin!4v1735298228605!5m2!1sen!2sin" 
-            width="100%" 
-            height="100%" 
-            style={{border:0}} 
-            allowFullScreen="" 
-            loading="lazy" 
-          >
-
-          </iframe>
-
+          </h2>
+          <p className="hidden md:flex mt-4 font-thin text-sm px-40">
+            Whether you’re looking to buy, sell, or rent, our experienced team
+            is here to guide you through <br></br> every step of the process.
+            Fill out the form below,and we’ll get <br></br> back to you promptly
+            with personalized assistance.
+          </p>
+          <Link to={"/"}>
+            {" "}
+            <button className="mt-6 px-4 py-2 bg-[#E7C873] text-sm text-black rounded-lg font-normal">
+              Back to Home &#8594;
+            </button>
+          </Link>
         </div>
       </div>
-      {/* Right Section - Office Location with Map */}
-      <div className="w-full md:w-1/2 p-4">
-        <h2 className="text-2xl font-semibold mb-4">Quick Enquiry</h2>
-        
-        <form className="space-y-4">
-          <div>
-            <label className="block text-sm 
-            font-medium text-gray-500" htmlFor="fullName">Full Name</label>
-            <input 
-              type="text" 
-              id="fullName" 
-              placeholder='enter your name'
-              className="w-full p-2 text-sm border-[1.5px] rounded-lg placeholder:text-[#1F4B43] border-[#1F4B43]" 
-              required 
+      <div className="flex flex-wrap bg-white p-8">
+        {/* Left Section - Form Inputs */}
+
+        <div className="w-full md:w-1/2 p-4">
+          <h2 className="text-2xl font-semibold mb-4">Our Office Location</h2>
+          <p>
+            C-401, Ganesh Glory 11,Nr. Moneyplant Highstreet,Jagatpur Road,
+            Gota, Ahmedabad-382481
+          </p>
+          <div className="map-container w-full h-64">
+            {/* Replace the coordinates with your office's latitude and longitude */}
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d7339.078095836263!2d72.538186!3d23.113964!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDA2JzUwLjMiTiA3MsKwMzInMjYuNyJF!5e0!3m2!1sen!2sin!4v1735298228605!5m2!1sen!2sin"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+        </div>
+        {/* Right Section - Office Location with Map */}
+        <div className="w-full md:w-1/2 p-4">
+          <h2 className="text-2xl font-semibold mb-4">Quick Enquiry</h2>
+
+          <form className="space-y-4">
+            <InputField
+              label="Full Name"
+              type="text"
+              placeholder="Enter your Name"
+              name="EnquiryPersonName"
+              value={formData.EnquiryPersonName}
+              onChange={handleInputChange}
+              required={true}
+              variant={3}
             />
-          </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-500" htmlFor="email">Email (Required)</label>
-            <input 
-              type="email" 
-              id="email" 
-              placeholder='enter your email id'
-              className="w-full p-2 text-sm border-[1.5px] rounded-lg placeholder:text-[#1F4B43] border-[#1F4B43]" 
-              required 
+            <InputField
+              label="Email (Required)"
+              type="text"
+              placeholder="Enter your Email id"
+              name="EnquiryPersonEmail"
+              value={formData.EnquiryPersonEmail}
+              onChange={handleInputChange}
+              required={true}
+              variant={3}
             />
-          </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-500" htmlFor="phone">Phone (Required)</label>
-            <input 
-              type="tel" 
-              id="phone" 
-                  placeholder='enter your contact number'
-              className="w-full p-2 text-sm border-[1.5px] rounded-lg placeholder:text-[#1F4B43] border-[#1F4B43]" 
-              required 
+            <InputField
+              label="Phone (Required)"
+              type="text"
+              placeholder="Enter your contact number"
+              name="EnquiryPersonPhone"
+              value={formData.EnquiryPersonPhone}
+              onChange={handleInputChange}
+              required={true}
+              variant={3}
             />
-          </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-500" htmlFor="message">Message</label>
-            <textarea 
-              id="message" 
-                  placeholder='enter details of enquiry'
-              className="w-full p-2 text-sm border-[1.5px] rounded-lg placeholder:text-[#1F4B43] border-[#1F4B43]" 
-              rows="4" 
-            ></textarea>
-          </div>
+            <InputField
+              label="Message"
+              type="textarea"
+              name="EnquiryPersonMessage"
+              placeholder="Enter Details of Enquiry"
+              value={formData.EnquiryPersonMessage}
+              onChange={handleInputChange}
+              required={true}
+              variant={3}
+            />
 
-          <button 
-            type="submit" 
-            className="mt-4 w-full bg-[#1F4B43] text-sm  text-white p-2 rounded-md "
-          >
-            Submit
-          </button>
-        </form>
+            <button
+              type="button"
+              onClick={handleSubmit}
+              className="mt-4 w-full bg-[#1F4B43] text-sm  text-white p-2 rounded-md "
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   );
 };

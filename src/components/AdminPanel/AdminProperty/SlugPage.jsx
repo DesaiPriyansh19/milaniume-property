27f2462bc9364fad1e899799217574e4862684a8
karan <jangdekaran54@gmail.com>
2025-01-12 16:44:25 +0530

    AdminSideBar

--- src/components/AdminPanel/AdminProperty/SlugPage.jsx
+++ src/components/AdminPanel/AdminProperty/SlugPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import MainDashBoard from "./MainDashBoard";
 import ManagePeoples from "./ManagePeoples";
 import ViewProperty from "./ViewProperty";
@@ -13,6 +13,12 @@ import RecycleBinProperty from "../RecycleBinProperty/RecycleBinProperty";
 
 export default function SlugPage() {
   const { slug } = useParams();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("userInfo");
+    navigate("/");
+  };
   return (
     <div className="w-full h-auto">
       {slug === "dashboard" && <MainDashBoard />}
@@ -22,6 +28,7 @@ export default function SlugPage() {
       {slug === "user-post" && <UserPostedProperty />}
       {slug === "require-post" && <UserRequirementPost />}
       {slug === "recycle-bin" && <RecycleBinProperty />}
+      {slug === "logout" && handleLogout()}
       {slug === "bar-chart" && <BarChart />}
       {slug === "pie-chart" && <PieChart />}
       {slug === "line-chart" && <LineChart />}
